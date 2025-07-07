b75e19b1e76a26fcd3a1cf508e538007d9c4ac6f
ciriast <ciriastorres@gmail.com>
2025-07-07 06:08:20 -0600

    For loop

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+void for_fun() {
+    for (int i = 0; i < 9; ++i) {
+        printf("Having fun \n");
+    }
+}
+
 int main() {
     printf("Hello friend, hello friend.\n");
+    for_fun();
 
     return 0;
 }
