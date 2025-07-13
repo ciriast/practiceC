70fbc3196aaec8c43c1aabe478cd491097334f18
ciriast <ciriastorres@gmail.com>
2025-07-13 07:58:08 -0600

    Second version, farenhait program

--- farenhait_celcius.c
+++ farenhait_celcius.c
@@ -3,7 +3,11 @@
 #define SUBSTRACT_VALUE 32
 
 int main() {
-    float farenhait_val = 68;
+    float farenhait_val = 0;
+
+    printf("Enter Farenhait: ");
+    scanf("%f", &farenhait_val);
+
     float celsiues_val = (farenhait_val - SUBSTRACT_VALUE) * (5.0f / 9.0f);
 
     printf("Celsiues equivalent: %f\n", celsiues_val);
