6394f351d98dcc767359c2e795404f9e19120b64
ciriast <ciriastorres@gmail.com>
2025-07-12 19:16:02 -0600

    New version

--- w_box.c
+++ w_box.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int volume = 12 * 10 * 8;
+    int height = 0;
+    int width = 0;
+    int length = 0;
+
+    printf("Enter the box height: ");
+    scanf("%d", &height);
+
+    printf("Enter the box width: ");
+    scanf("%d", &width);
+
+    printf("Enter the box length: ");
+    scanf("%d", &length);
+
+    int volume = length * height * width;
     int weight = (volume + 165) / 166;
 
     printf("Weight: %d\n", weight);
