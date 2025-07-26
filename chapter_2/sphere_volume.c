ecdbd39b3823a074ea35d24a09e1983cd89c059d
ciriast <ciriastorres@gmail.com>
2025-07-26 16:29:38 -0600

    sphere volume project version 2

--- chapter_2/sphere_volume.c
+++ chapter_2/sphere_volume.c
@@ -3,7 +3,12 @@
 const float pi_value = 3.1416f;
 
 int main() {
-    float volume = (4.0f/3.0f) * pi_value * (10*10*10);
+    float sphere_radius = 0;
+
+    printf("Enter the sphere radius:\n");
+    scanf("%f", &sphere_radius);
+
+    float volume = (4.0f/3.0f) * pi_value * (sphere_radius * sphere_radius * sphere_radius);
     printf("The sphere volume is: %f\n", volume);
     
     return 0;
