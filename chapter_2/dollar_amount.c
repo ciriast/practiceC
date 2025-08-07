6e6654737389aaa5411b959ece66cb414e711c10
ciriast <ciriastorres@gmail.com>
2025-08-06 18:24:56 -0600

    Add bill const

--- chapter_2/dollar_amount.c
+++ chapter_2/dollar_amount.c
@@ -4,6 +4,11 @@ int main() {
 
     float user_amount = 0;
 
+    const int twenty_bill = 20;
+    const int ten_bill = 10;
+    const int five_bill = 5;
+    const int one_bill = 1;
+
     printf("Enter a dollar amount: ");
     scanf("%f", &user_amount);
 
