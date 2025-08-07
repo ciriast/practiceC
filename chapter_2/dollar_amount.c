9d535f590a68a1117c18ad37e9d2f223b60f538a
ciriast <ciriastorres@gmail.com>
2025-08-07 06:21:02 -0600

    Continue with the project, it's almost done

--- chapter_2/dollar_amount.c
+++ chapter_2/dollar_amount.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void show_bill_message(int twenty_bill_count, int ten_bill_count, int five_bill_count, int one_bill_count) {
+
+    printf("$20 bills: %d\n", twenty_bill_count);
+    printf("$10 bills: %d\n", ten_bill_count);
+    printf("$5 bills: %d\n", five_bill_count);
+    printf("$1 bills: %d\n", one_bill_count);
+
+}
+
 int main() {
 
     float user_amount = 0;
@@ -11,6 +20,21 @@ int main() {
 
     printf("Enter a dollar amount: ");
     scanf("%f", &user_amount);
+    
+    int amount_after_substraction = user_amount;
+
+    const int twenty_bill_count = user_amount / twenty_bill;
+    amount_after_substraction = amount_after_substraction - (twenty_bill_count * twenty_bill);
+
+    int ten_bill_count = 0;
+    int five_bill_count = 0;
+    int one_bill_count = 0;
+
+    if (amount_after_substraction <= 0) {
+        show_bill_message(twenty_bill_count, ten_bill_count, five_bill_count, one_bill_count);
+
+        return 0;
+    }
 
     return 0;
 }
