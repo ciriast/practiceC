f74287e28f70209de1a69cdef3de016df3ad0e37
ciriast <ciriastorres@gmail.com>
2025-08-09 11:55:27 -0600

    Continue balance project

--- chapter_2/balance.c
+++ chapter_2/balance.c
@@ -4,9 +4,16 @@ int main() {
     
     float loan_amount = 0;
     const float monthly_payment = 386.66f;
+    float annual_interest = 0;
+    float annual_interest_converted = 0;
 
     printf("Enter amount of loan: ");
     scanf("%f", &loan_amount);
+
+    printf("Enter interest rate: ");
+    scanf("%f", &annual_interest);
+
+    printf("Monthly payment: %.2f\n", monthly_payment);
 
     return 0;
 }
