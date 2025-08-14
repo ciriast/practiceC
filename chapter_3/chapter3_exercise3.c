a591fe16aa06c3505329580133cab91cfa1b970f
ciriast <ciriastorres@gmail.com>
2025-08-14 07:40:43 -0600

    Exercise 3 almost done

--- chapter_3/chapter3_exercise3.c
+++ chapter_3/chapter3_exercise3.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    printf("%d\n", 120);
+    printf(" %d\n", 120);
+
+    printf("%d-%d-%d\n", 120, 120, 120);
+    printf("%d -%d -%d\n", 120, 120, 120);
+
+    printf("%f\n", 120.1);
+    printf("%f \n", 120.1);
+
     return 0;
 }
