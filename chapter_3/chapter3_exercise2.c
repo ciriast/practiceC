fec926c5be17a01167e278459061693758887649
ciriast <ciriastorres@gmail.com>
2025-08-13 20:19:36 -0600

    Exercise 2 completed

--- chapter_3/chapter3_exercise2.c
+++ chapter_3/chapter3_exercise2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    printf("%8.1e\n", 30.253);
+    printf("%10.6e\n", 30.253);
+    printf("%8.3f\n", 30.253);
+    printf("%6.0f\n", 30.253);
+
     return 0;
 }
