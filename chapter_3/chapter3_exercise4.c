0eafe440a9d4e0d68e9321436d99752d25a9f03c
ciriast <ciriastorres@gmail.com>
2025-08-14 20:52:48 -0600

    Exercise 4 finished

--- chapter_3/chapter3_exercise4.c
+++ chapter_3/chapter3_exercise4.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main() {
+    int i = 0;
+    int j = 0;
+    float x = 0;
+
+    printf("Enter the values for i, j and x\n");
+    scanf("%d%f%d", &i, &x, &j);
+
+    printf("x: %f\n", x);
+    printf("i: %d\n", i);
+    printf("j: %d\n", j);
+
     return 0;
 }
