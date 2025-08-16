bc25de3eb744a6b212c0be815509d847b9be902e
ciriast <ciriastorres@gmail.com>
2025-08-16 15:21:31 -0600

    Exercise 5 finished

--- chapter_3/chapter3_exercise5.c
+++ chapter_3/chapter3_exercise5.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    float x = 0;
+    int i = 0;
+    float y = 0;
+
+    printf("Enter the values for x, i and y: \n");
+    scanf("%f%d%f", &x, &i, &y);
+
+    printf("x value: %f\n", x);
+    printf("i value: %d\n", i);
+
     return 0;
 }
