98f16972e5eefebf0962a171d23bd5139dc84310
ciriast <ciriastorres@gmail.com>
2025-08-22 07:09:28 -0600

    Find the user day

--- chapter_3/projects/project1.c
+++ chapter_3/projects/project1.c
@@ -7,8 +7,9 @@ int main() {
     scanf("%79s", user_date);
 
     char user_month[3] = {user_date[0], user_date[1], '\0'};
+    char user_day[3] = {user_date[3], user_date[4], '\0'};
 
-    printf("You entered the month: %s\n", user_month);
+    printf("You entered the day: %s\n", user_day);
 
     return 0;
 }
