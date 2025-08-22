e10f273ceacbfc5ad3963c7ec63b42a7a1cf5b47
ciriast <ciriastorres@gmail.com>
2025-08-22 06:52:44 -0600

    Able to get the month from string

--- chapter_3/projects/project1.c
+++ chapter_3/projects/project1.c
@@ -2,11 +2,13 @@
 
 int main() {
     char user_date[80];
-
+    
     printf("Enter a date (mm/dd/yyyy): ");
     scanf("%79s", user_date);
 
-    printf("You entered the date: %s\n", user_date);
+    char user_month[3] = {user_date[0], user_date[1], '\0'};
+
+    printf("You entered the month: %s\n", user_month);
 
     return 0;
 }
