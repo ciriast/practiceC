2150ae0b5e92b44d87b57faba36fa4eba7ff2478
ciriast <ciriastorres@gmail.com>
2025-08-22 21:20:22 -0600

    Get the year

--- chapter_3/projects/project1.c
+++ chapter_3/projects/project1.c
@@ -8,8 +8,9 @@ int main() {
 
     char user_month[3] = {user_date[0], user_date[1], '\0'};
     char user_day[3] = {user_date[3], user_date[4], '\0'};
+    char user_year[5] = {user_date[6], user_date[7], user_date[8], user_date[9], '\0'};
 
-    printf("You entered the day: %s\n", user_day);
+    printf("You entered the year: %s\n", user_year);
 
     return 0;
 }
