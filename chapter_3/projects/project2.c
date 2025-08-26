f055f47133034073508adbcb59893d2f98773e95
ciriast <ciriastorres@gmail.com>
2025-08-25 19:13:09 -0600

    Ready to get the user info

--- chapter_3/projects/project2.c
+++ chapter_3/projects/project2.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    int item_number;
+    float item_price;
+    char item_purchase_date[30];
+
+    printf("Enter item number: ");
+    scanf("%d", &item_number);
+
+    printf("Enter unit price: ");
+    scanf("%f", &item_price);
+
+    printf("Enter purchase date (mm/dd/yyyy): ");
+    scanf("%s", item_purchase_date);
+
     return 0;
 } 
