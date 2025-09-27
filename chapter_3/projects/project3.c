f2d99570d531e5bad80c4d17560be2c73e701d68
ciriast <ciriastorres@gmail.com>
2025-09-26 19:15:11 -0600

    For now this is working

--- chapter_3/projects/project3.c
+++ chapter_3/projects/project3.c
@@ -15,17 +15,14 @@ int main() {
     int text_length_val = text_length(user_isbn);
         
     int first_index = find_in_text(0, user_isbn, text_length_val);
-    int group_identifier_index = find_in_text(first_index, user_isbn, text_length_val);
+    int what_i_need = first_index + 1;
+    int group_identifier_index = find_in_text(what_i_need, user_isbn, text_length_val);
     int publisher_index = find_in_text(group_identifier_index, user_isbn, text_length_val);
 
-    int testing = publisher_index - group_identifier_index + 1;
-    printf("testing the val: %d\n", testing);
-
-    //printf("group index: %d\n", group_identifier_index);
-    //printf("publisher_index: %d\n", publisher_index);
+    int testing = group_identifier_index - what_i_need;
 
     char *gs1_prefix = get_piece_of_text(0, first_index, user_isbn);
-    char *group_identifier = get_piece_of_text(first_index, testing, user_isbn);
+    char *group_identifier = get_piece_of_text(what_i_need, testing, user_isbn);
 
     printf("GS1 prefix: %s\n", gs1_prefix);
     printf("Group identifier: %s\n", group_identifier);
@@ -37,7 +34,7 @@ int main() {
 }
 
 int find_in_text(int start_index, char *text, int text_length) {
-   
+ 
     for(int i = start_index; i < text_length; i++) {
         if (text[i] == '-')
             return i;
@@ -63,6 +60,7 @@ char *get_piece_of_text(int start_index, int buffer, char *text) {
 
     for (int i = 0; i < length_to_copy; ++i) {
         piece_of_text[i] = text[start_index];
+
         start_index++;
     }
 
