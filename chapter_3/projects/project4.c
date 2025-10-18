6033f69c43cbbda3681f58cf5df47ad91d8a4b46
carlos <ciriastorres@gmail.com>
2025-10-18 09:52:22 -0600

    Working in the logic

--- chapter_3/projects/project4.c
+++ chapter_3/projects/project4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int get_string_length(char *string);
+int get_string_length(char *user_data);
 
 int main() {
     char phone_number[50];
@@ -8,8 +8,23 @@ int main() {
     printf("Enter your phone number: ");
     scanf("%s", phone_number);
 
+    int user_data_length = get_string_length(phone_number);
+
+    printf("user data length: %d\n", user_data_length);
+
     // my first comment from my gnu/linux laptop!
     // ok, now I have vim setup in my laptop
     // Look, I'm trying to get better
     return 0;
 }
+
+int get_string_length(char *user_data) {
+    int counter = 0;
+
+    while(user_data[counter] != '\0') {
+        counter ++;
+    }
+
+    return counter;
+}
+    
