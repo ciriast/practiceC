b815b70ccbe3846309a6ca582c56200361548889
carlos <ciriastorres@gmail.com>
2025-10-18 10:11:16 -0600

    Project 4 finished

--- chapter_3/projects/project4.c
+++ chapter_3/projects/project4.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int get_string_length(char *user_data);
+char *replace_character(int buffer, char *user_data);
 
 int main() {
     char phone_number[50];
@@ -10,8 +12,11 @@ int main() {
 
     int user_data_length = get_string_length(phone_number);
 
-    printf("user data length: %d\n", user_data_length);
+    char *replaced_text = replace_character(user_data_length, phone_number);
 
+    printf("The user phone is: %s\n", replaced_text);
+
+    free(replaced_text);
     // my first comment from my gnu/linux laptop!
     // ok, now I have vim setup in my laptop
     // Look, I'm trying to get better
@@ -27,4 +32,20 @@ int get_string_length(char *user_data) {
 
     return counter;
 }
-    
+ 
+char *replace_character(int buffer, char *user_data) {
+    int buffer_with_end = buffer + 1;
+    char *my_new_text = (char*) malloc(buffer_with_end);
+
+    for(int i = 0; i < buffer; ++i){
+        my_new_text[i] = user_data[i];
+
+        if (my_new_text[i] == '-') {
+            my_new_text[i] = '.';
+        }
+    }
+
+    my_new_text[buffer_with_end] = '\0';
+
+    return my_new_text;
+}
