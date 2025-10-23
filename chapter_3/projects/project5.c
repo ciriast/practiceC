deaa90b2b43cdd32e3912aeaf306511be9883f29
carlos <ciriastorres@gmail.com>
2025-10-22 21:09:14 -0600

    Solve all the bugs until now

--- chapter_3/projects/project5.c
+++ chapter_3/projects/project5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void print_array(int items[3]);
+void print_array(int items[4]);
 
 int main() {
-    int numbers_from_user[15];
+    int numbers_from_user[16];
     int arragment = 4;
-    int number_groups [3][3];
+    int number_groups [4][4];
 
     printf("Enter the numbers from 1 to 16 in any order: ");
     scanf("%d", &numbers_from_user[0]);
@@ -28,24 +28,27 @@ int main() {
     int control_row = 0;
     int control_column = 1;
 
-    for (int i = 0; i < 15; ++i) {
+    for (int i = 0; i < 16; ++i) {
 
         number_groups[control_row][control_column - 1] = numbers_from_user[i];
 
         control_column++;
 
-        if (control_column == 4) {
+        if (control_column == 5) {
             control_row++;
             control_column = 1;
         }
     }
 
     print_array(number_groups[0]);
+    print_array(number_groups[1]);
+    print_array(number_groups[2]);
+    print_array(number_groups[3]);
 
     return 0;
 }
 
-void print_array(int items[3]) {
+void print_array(int items[4]) {
     for (int i = 0; i < 4; ++i) {
         printf(" %d", items[i]);
     }
