e74e3b9d06baebe747edfd7d1f79a1b495e97c5b
carlos <ciriastorres@gmail.com>
2025-11-12 20:23:35 -0600

    first version

--- chapter_4/check_digit_UPC.c
+++ chapter_4/check_digit_UPC.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hi, friend");
+    
+    int first_digit;
+    int first_group_digit;
+    int second_group_digit;
+
+    printf("Enter the first (single) digit: ");
+    scanf("%d", &first_digit);
+
+    printf("Enter first group of five digits: ");
+    scanf("%d", &first_group_digit);
+
+    printf("Enter second group of five digits: ");
+    scanf("%d", &second_group_digit);
+
+    printf("Check digit: %d\n", 5);
 
     return 0;
 }
