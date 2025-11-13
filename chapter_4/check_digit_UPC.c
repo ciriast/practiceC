b13f4621833443dcfe7ee318a444d3e04473d2d6
carlos <ciriastorres@gmail.com>
2025-11-12 20:40:17 -0600

    Continue with example

--- chapter_4/check_digit_UPC.c
+++ chapter_4/check_digit_UPC.c
@@ -3,17 +3,20 @@
 int main() {
     
     int first_digit;
-    int first_group_digit;
-    int second_group_digit;
+    int i1, i2, i3, i4, i5;
+    int j1, j2, j3, j4, j5;
+    int first_sum;
+    int second_sum;
+    int total;
 
     printf("Enter the first (single) digit: ");
-    scanf("%d", &first_digit);
+    scanf("%1d", &first_digit);
 
     printf("Enter first group of five digits: ");
-    scanf("%d", &first_group_digit);
+    scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
 
     printf("Enter second group of five digits: ");
-    scanf("%d", &second_group_digit);
+    scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
 
     printf("Check digit: %d\n", 5);
 
